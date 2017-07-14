a8100e90ec0ad6557164fc41fe5cac1db7aeba16
oggonzalezm <ogm@soluciones4g.com>
2017-07-14 15:39:28 +0000

    [REF]

--- cmt_trust/models/trust.py
+++ cmt_trust/models/trust.py
@@ -10,7 +10,7 @@ class Trust(models.Model):
 		 'x_cmt_instalauidmismo',
 		 'x_cmt_cuentaconpersonal',
 		 'x_cmt_solovendeotmbninstala',
-		 'x_cmt_quetipodebienes')
+		 'x_cmt_quetipodebienes',)
 	def _get_trust(self):
-	    for record in self:
+		for record in self:
     		record['x_cmt_trust'] = self.x_cmt_tambienconstruye + self.x_cmt_instalauidmismo + self.x_cmt_cuentaconpersonal + self.x_cmt_solovendeotmbninstala + self.x_cmt_quetipodebienes
